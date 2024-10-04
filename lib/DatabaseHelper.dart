1b2946cee55ae3a4f573a4aac23e8a55da07b37c
user1 <valhallamaia@gmail.com>
2024-10-04 20:32:56 -0300

    nao entendi nadaaaa

--- lib/DatabaseHelper.dart
+++ lib/DatabaseHelper.dart
@@ -8,5 +8,33 @@ class DatabaseHelper {
     return _instance;
   }
   DatabaseHelper. _internal();
+  
+  Future<Database> get database async{
+    if(_database != null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async{
+    String dbPath = await getDatabasesPath();
+    return openDatabase(
+      
+      join(dbPath, 'queroirembora.db'),
+      version: 1,
+      onCreate: _onCreate,
+      
+      );
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute('CREATE TABLE pessoa(id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, cpf TEXT, creditcard TEXT)');
+  }
+
+
+  Future close() async{
+    final db = await database;
+    db.close();
+  }
+
 
 }
